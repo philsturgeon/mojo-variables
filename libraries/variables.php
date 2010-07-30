9525095ef527623eaac11ece12b9151bc553c1ac
Phil Sturgeon <email@philsturgeon.co.uk>
2010-07-30 09:47:58 +0100

    Added library use {mojo:variables:set name=foo value=bar} and {mojo:variables:get name=foo}.

---
+++ libraries/variables.php
@@ -0,0 +1,83 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Variable addon
+ *
+ * Set variables in your layout file or anywhere throughout Mojo Regions then
+ * use them again anywhere.
+ *
+ * @package		MojoMotor
+ * @subpackage	Addons
+ * @author		Phil Sturgeon
+ * @link		http://philsturgeon.co.uk/code/mojomotor/variables
+ */
+class Variables
+{
+	public $addon;
+	public $addon_version = '1.0';
+
+	private $_variables = array();
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Constructor
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	function __construct()
+	{
+		$this->addon =& get_instance();
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Set
+	 *
+	 * Set a variable.
+	 *
+	 * @access	public
+	 * @param	array
+	 * @return	string
+	 */
+	function set($template_data = array())
+	{
+		$params =& $template_data['parameters'];
+
+		if ( ! isset($params['name']) OR ! isset($params['value']))
+		{
+			return;
+		}
+
+		$this->_variables[$params['name']] = $params['value'];
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Set
+	 *
+	 * Set a variable.
+	 *
+	 * @access	public
+	 * @param	array
+	 * @return	string
+	 */
+	function get($template_data = array())
+	{
+		$params =& $template_data['parameters'];
+
+		if ( ! isset($params['name']))
+		{
+			return;
+		}
+
+		return $this->_variables[$params['name']];
+	}
+
+}
+
+/* End of file robots.php */
+/* Location: system/mojomotor/third_party/robots/libraries/robots.php */
